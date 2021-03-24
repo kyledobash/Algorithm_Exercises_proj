80cb8c78bf67bcfe2d254e5ca57287e77f25d7b7
kyledobash <kyledobash@yahoo.com>
2021-03-24 12:04:58 -0700

    phone number challenge solved

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def create_phone_number(n):
+    number_array = []
+    number_array.append("(")
+    number_array.append(n[0])
+    number_array.append(n[1])
+    number_array.append(n[2])
+    number_array.append(")")
+    number_array.append(" ")
+    number_array.append(n[3])
+    number_array.append(n[4])
+    number_array.append(n[5])
+    number_array.append("-")
+    number_array.append(n[6])
+    number_array.append(n[7])
+    number_array.append(n[8])
+    number_array.append(n[9])
+
+    phone_number_array = map(str, number_array)
+    # phone_number.join(number_array)
+
+    # phone_number = ''.join(number_array)
+
+    phone_number = "".join(phone_number_array)
+
+    return phone_number
+
+
+print(create_phone_number([6, 0, 2, 9, 9, 9, 4, 2, 9, 8]))
