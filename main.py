f9269237ed96e29a9a9f43e85a7f44ea75a1eb36
kyledobash <kyledobash@yahoo.com>
2021-03-24 12:24:19 -0700

    completed count bits exercise

--- main.py
+++ main.py
@@ -37,3 +37,18 @@ def create_phone_number_refactored(n):
 
 print(create_phone_number_refactored([6, 0, 2, 9, 9, 9, 4, 2, 9, 8]))
 
+
+def count_bits(n):
+    counter = 0
+    input_binary = bin(n)
+    for digit in input_binary:
+        if digit == "1":
+            counter += 1
+
+    return counter
+
+
+print(count_bits(0))
+print(count_bits(4))
+print(count_bits(7))
+print(count_bits(9))
