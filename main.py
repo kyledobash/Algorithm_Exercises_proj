9c36560309acd89e1530128e19ff9212aa0a43fa
kyledobash <kyledobash@yahoo.com>
2021-03-24 12:12:02 -0700

    refactored phone number challenge

--- main.py
+++ main.py
@@ -19,13 +19,21 @@ def create_phone_number(n):
     number_array.append(n[9])
 
     phone_number_array = map(str, number_array)
-    # phone_number.join(number_array)
-
-    # phone_number = ''.join(number_array)
-
     phone_number = "".join(phone_number_array)
 
     return phone_number
 
 
-print(create_phone_number([6, 0, 2, 9, 9, 9, 4, 2, 9, 8]))
+def create_phone_number_refactored(n):
+    n.insert(0, "(")
+    n.insert(4, ") ")
+    n.insert(8, "-")
+
+    phone_number_array = map(str, n)
+    phone_number = "".join(phone_number_array)
+
+    return phone_number
+
+
+print(create_phone_number_refactored([6, 0, 2, 9, 9, 9, 4, 2, 9, 8]))
+
